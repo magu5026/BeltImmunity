28e1d658e9eb5bd83c164192ec0a901cf532059f
magu5026 <magu5026@gmail.com>
2018-05-27 17:51:27 +0200

    release for factorio 0.16

--- data.lua
+++ data.lua
@@ -1,46 +1,19 @@
-data:extend(
-{
-  {
-    type = "technology",
-    name = "belt-immunity-equipment",
-    icon = "__BeltImmunity__/graphics/belt-immunity-equipment.png",
-	icon_size = 128,
-	prerequisites = {"modular-armor"},
-    effects =
-    {
-      {
-        type = "unlock-recipe",
-        recipe = "belt-immunity-equipment"
-      }
-    },
-    unit =
-    {
-      count = 50,
-      ingredients = {{"science-pack-1", 1}, {"science-pack-2", 1}},
-      time = 15
-    },
-    order = "g-g-b"
-  },
-  
-  {
-    type = "recipe",
-    name = "belt-immunity-equipment",
-    enabled = false,
-    energy_required = 10,
-	ingredients = 
+local bie_tech = table.deepcopy(data.raw['technology']['night-vision-equipment'])
+bie_tech.name = "belt-immunity-equipment"
+bie_tech.icon = "__BeltImmunity__/graphics/belt-immunity-equipment.png"
+bie_tech.effects = 
 	{
-      {"advanced-circuit", 5},
-      {"steel-plate", 10}
-    },
-    result = "belt-immunity-equipment"
-  },
-  
- })
- 
- 
- data.raw["belt-immunity-equipment"]['belt-immunity-equipment'].shape = 
-	{
-      width = 1,
-      height = 2,
-      type = "full"
+		{
+			type = "unlock-recipe",
+			recipe = "belt-immunity-equipment"
+		}
     }
+bie_tech.order = "g-g-b"
+
+
+local bie_rec = table.deepcopy(data.raw['recipe']['night-vision-equipment'])
+bie_rec.name = "belt-immunity-equipment"
+bie_rec.result = "belt-immunity-equipment"
+
+
+data:extend({bie_tech,bie_rec})
